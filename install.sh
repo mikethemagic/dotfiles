d264b6a6b0a4d9a0257808c9e100a1b41969362f
Michael Stangl <michael.stangl@gmail.com>
2013-01-22 08:04:07 +0100

    - weiter an Integration der .vimrc files gebastelt

--- install.sh
+++ install.sh
@@ -1,7 +1,41 @@
 #!/bin/bash
+MYDIR="`dirname $(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")`"
 
-function abspath() { echo `pwd`/$1; }
+lnif() {
+    if [ ! -e $2 ] ; then
+        ln -s $1 $2
+    fi
+    if [ -L $2 ] ; then
+        ln -sf $1 $2
+    fi
+}
 
-ln -s `abspath .gitignore` ~/.gitignore
-ln -s `abspath .gitconfig` ~/.gitconfig
+# Backup existing .vim stuff
+echo "backing up current vim config"
+today=`date +%Y%m%d`
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
 
+# linking .vim configs
+echo "setting up symlinks"
+FILES=".gitignore .gitconfig .vim .vimrc .gvimrc .vimrc.bundles.fork .vimrc.bundles.local .vimrc.fork"
+for i in $FILES; do
+    echo $i
+    lnif $MYDIR/$i $HOME/$i
+done
+
+if [ ! -d $MYDIR/.vim/bundle ]; then
+    mkdir -p $MYDIR/.vim/bundle
+fi
+
+if [ ! -e $HOME/.vim/bundle/vundle ]; then
+    echo "Installing Vundle"
+    git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+fi
+
+
+
+echo "update/install plugins using Vundle"
+system_shell=$SHELL
+export SHELL="/bin/sh"
+vim -u $MYDIR/.vimrc.bundles +BundleInstall! +BundleClean +qall
+export SHELL=$system_shell
--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+MYDIR="`dirname $(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")`"
 function dellink() {
     if [ -L $1 ] ; then
         rm $1
     fi
 }
 
-dellink ~/.vim
 dellink ~/.vimrc
+dellink ~/.gvimrc
+dellink ~/.vimrc.bundles
+dellink ~/.vimrc.fork
+dellink ~/.vimrc.local
+dellink ~/.vimrc.bundles.fork
+dellink ~/.vim
+
+rm -rf $MYDIR/.vim
+
